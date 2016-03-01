3248c411967e2a37d591671c062e85e5cbcd0e81
Brittany <leeabrittany@gmail.com>
2016-03-01 14:46:14 -0800

    Create template file

---
+++ template.py
@@ -0,0 +1,64 @@
+from random import randint
+
+def sports_words():
+	return ['athletics', 'baseball', 'basketball', 'bowling', 'cycling', 'football', 'golf', 'gymnastics', 'handball', 'hockey', 'jogging', 'polo', 'rugby', 'skiing', 'soccer', 'softball', 'squash', 'swimming', 'tennis', 'volleyball']
+
+def hangman_game():
+	# choose word that the user will guess
+	words = sports_words()
+	number = randint(0,len(words)-1)
+	play_word = words[number]
+	word_length = len(play_word)
+
+	# set the number of tries the user has
+	tries = 7
+
+	print
+	print 'Welcome to Hangman created by Brittany'
+	print 'Try to guess this sports related word:'
+	print
+
+	answer = ['_ '] * word_length
+	print ''.join(answer)
+
+	# loop while the user still has tries left
+	while tries > 0:
+		guess_letter = raw_input('Guess a letter: ')
+
+		# loop through each letter in the play word
+		for i in range(0,word_length):
+			# check if the user guessed a letter in the play word
+			if play_word[i] == guess_letter:
+				answer[i] = guess_letter + ' '  
+
+		print ''.join(answer)
+
+		# check if the user has a guess for the word
+		guess = raw_input('Do you know the word? (y/n) ')
+		if guess == 'y':
+			guess_word = raw_input('What is your guess? ')
+			if guess_word == play_word:
+				# if the user guessed correctly, they won
+				print 'You won!'
+				break
+			else:
+				# if the user guessed incorrectly, they lose a try
+				print 'Not quite, but keep trying!'
+				tries-=1
+		else:
+			# if the user does not guess the word, they lose a try
+			tries-=1
+
+	# if the user is all out of guesses they lose the game
+	if tries <= 0:
+		print 'Out of guesses :('
+		print 'The word was ' + play_word
+
+	# ask the user if they want to play again
+	play_again = raw_input('Do you want to play again? (y/n) ')
+	if play_again == 'y':
+		hangman_game()
+
+hangman_game()
+
+
